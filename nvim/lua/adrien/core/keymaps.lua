466ff3624cd56f119e4f8361e90279edda473526
Adrien Verriele <adrien.verriele08@gmail.com>
2025-04-23 19:23:38 +0200

    Added distant.nvim

--- nvim/lua/adrien/core/keymaps.lua
+++ nvim/lua/adrien/core/keymaps.lua
@@ -21,3 +21,10 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set(
+	"n",
+	"<leader>dc",
+	"<cmd>DistantConnect ssh://student@es-pynq041.ics.ele.tue.nl<CR>",
+	{ desc = "Connect to remote server" }
+)
+keymap.set("n", "<leader>do", "<cmd>DistantOpen<CR>", { desc = "Open remote server home directory" })
